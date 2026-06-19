24bccb14acd7092b3df1f3c4b8ce1315afd9771a
Tanvir <tanvirahamed.foysal.00@gmail.com>
2026-06-19 14:30:01 +0600

    Update TOML file for deploying

--- Backend/main.py
+++ Backend/main.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "app.main:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=settings.debug,
